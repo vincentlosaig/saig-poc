15ac197b1f1824432188d7a25f4c15590bd898ed
unknown <lo0vin0@AUSYDHQ-COTWE01.saig.frd.global>
2014-06-17 13:55:48 +1000

    Added textarea and radio button functionality. Changed checkbox to show Yes and No. Display online status.

--- public/js/init.js
+++ public/js/init.js
@@ -1,7 +1,32 @@
 $(document).ready(function() {
+
+	var isOnline;
+	
 	$(function(){
 		$("[data-hide]").on("click", function(){
 			$("." + $(this).attr("data-hide")).hide(600);
 		});
 	});
+	
+	function reportOnlineStatus() {
+		isOnline = navigator.onLine;
+		
+		if (isOnline) {
+			$("#onlineStatus").removeClass("btn-danger").addClass("btn-success");
+		} else {			
+			$("#onlineStatus").removeClass("btn-success").addClass("btn-danger");
+		}
+	}
+	
+	if (window.applicationCache) {
+		window.addEventListener("online", function(e) {
+			reportOnlineStatus();
+		}, true);
+		
+		window.addEventListener("offline", function(e) {
+			reportOnlineStatus();
+		}, true);
+		
+		reportOnlineStatus();
+	}
 });
